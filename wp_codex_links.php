6ddad105894edf061910e7faaaef48d32dcc3b0a
atachibana <unofficialtokyo@gmail.com>
2015-07-12 16:30:52 +0900

    Modfiled to JP pages that I translated.
    I translated all non-translated pages :-)

--- wp_codex_links.php
+++ wp_codex_links.php
@@ -1,15 +1,15 @@
 <?php
 $wp_codex_links = array(
 
-							 'archive_page' => 'http://codex.wordpress.org/Creating_an_Archive_Index#The_Archives_Page',
+							 'archive_page' => 'http://wpdocs.sourceforge.jp/Creating_an_Archive_Index#The_Archives_Page',
 							
-							 'singular_page' => 'http://codex.wordpress.org/Post_Type_Templates',
+							 'singular_page' => 'http://wpdocs.sourceforge.jp/Post_Type_Templates',
 							
 							 'site_front_page' => 'http://wpdocs.sourceforge.jp/Creating_a_Static_Front_Page',
 							
-							 'blog_posts_index_page' => 'http://codex.wordpress.org/Settings_Reading_Screen#Settings_.E2.86.92_Reading',
+							 'blog_posts_index_page' => 'http://wpdocs.sourceforge.jp/%E7%AE%A1%E7%90%86%E7%94%BB%E9%9D%A2/%E8%A1%A8%E7%A4%BA%E8%A8%AD%E5%AE%9A#.E8.A1.A8.E7.A4.BA.E8.A8.AD.E5.AE.9A',
 							
-							 'comments_popup_page' => 'http://codex.wordpress.org/Function_Reference/comments_popup_script',
+							 'comments_popup_page' => 'http://wpdocs.sourceforge.jp/%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88%E3%82%BF%E3%82%B0/comments_popup_script',
 							
 							 'error_404_page' => 'http://wpdocs.sourceforge.jp/Creating_an_Error_404_Page',
 								
@@ -21,17 +21,17 @@ $wp_codex_links = array(
 								
 							 'front_page' => 'http://wpdocs.sourceforge.jp/Creating_a_Static_Front_Page',
 																
-							 'author_archive' => 'http://codex.wordpress.org/Author_Templates',
+							 'author_archive' => 'http://wpdocs.sourceforge.jp/Author_Templates',
 							
 							 'category_archive' => 'http://wpdocs.sourceforge.jp/Category_Templates',
 							
-							 'custom_post_type_archive' => 'http://codex.wordpress.org/Post_Type_Templates',
+							 'custom_post_type_archive' => 'http://wpdocs.sourceforge.jp/Post_Type_Templates',
 							
-							 'custom_taxonomy_archive' => 'http://codex.wordpress.org/Post_Type_Templates',
+							 'custom_taxonomy_archive' => 'http://wpdocs.sourceforge.jp/Post_Type_Templates',
 								
-							 'date_archive' => 'http://codex.wordpress.org/Function_Reference/wp_get_archives#Examples',
+							 'date_archive' => 'http://wpdocs.sourceforge.jp/%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88%E3%82%BF%E3%82%B0/wp_get_archives#Examples',
 								
-							 'tag_archive' => 'http://codex.wordpress.org/Tag_Templates',
+							 'tag_archive' => 'http://wpdocs.sourceforge.jp/Tag_Templates',
 							
 							 'attachment_post' => 'http://wpdocs.sourceforge.jp/Template_Hierarchy#Attachment_display',
 								
@@ -45,7 +45,7 @@ $wp_codex_links = array(
 								
 							 'posts_shown_on_front' => 'http://wpdocs.sourceforge.jp/Template_Hierarchy#Front_Page_display',
 								
-							 'author_nicename' => 'http://codex.wordpress.org/Author_Templates',
+							 'author_nicename' => 'http://wpdocs.sourceforge.jp/Author_Templates',
 								
 							 'category_slug' => 'http://wpdocs.sourceforge.jp/Category_Templates',
 								
@@ -57,66 +57,66 @@ $wp_codex_links = array(
 								
 							 'day_archive' => 'http://wpdocs.sourceforge.jp/Template_Hierarchy#Date_display',
 								
-							 'tag_slug' => 'http://codex.wordpress.org/Tag_Templates',
+							 'tag_slug' => 'http://wpdocs.sourceforge.jp/Tag_Templates',
 								
-							 'mimetype' => 'http://codex.wordpress.org/Function_Reference/get_post_mime_type',
+							 'mimetype' => 'http://wpdocs.sourceforge.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/get_post_mime_type',
 								
-							 'custom_template' => 'http://codex.wordpress.org/Page_Templates#Custom_Page_Template',
+							 'custom_template' => 'http://wpdocs.sourceforge.jp/Page_Templates#Custom_Page_Template',
 								
 							 'default_template' => 'http://wpdocs.sourceforge.jp/Post_Types#Page',
 								
-							 'author_id' => 'http://codex.wordpress.org/Author_Templates',
+							 'author_id' => 'http://wpdocs.sourceforge.jp/Author_Templates',
 								
-							 'category_id' => 'http://codex.wordpress.org/Category_Templates',
+							 'category_id' => 'http://wpdocs.sourceforge.jp/Category_Templates',
 								
-							 'taxonomy_taxonomy' => 'http://codex.wordpress.org/Post_Type_Templates',
+							 'taxonomy_taxonomy' => 'http://wpdocs.sourceforge.jp/Post_Type_Templates',
 							
-							 'tag_id' => 'http://codex.wordpress.org/Tag_Templates',
+							 'tag_id' => 'http://wpdocs.sourceforge.jp/Tag_Templates',
 								
 							 'subtype' => 'http://wpdocs.sourceforge.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/get_post_mime_type',
 								
-							 'custom' => 'http://codex.wordpress.org/Page_Templates#Custom_Page_Template',
+							 'custom' => 'http://wpdocs.sourceforge.jp/Page_Templates#Custom_Page_Template',
 							
 							 'mimetype_subtype' => 'http://wpdocs.sourceforge.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/get_post_mime_type',
 							
-							 'page_slug' => 'http://codex.wordpress.org/Page_Templates#Specialized_Page_Template',
+							 'page_slug' => 'http://wpdocs.sourceforge.jp/Page_Templates#Specialized_Page_Template',
 							
-							 'author' => 'http://codex.wordpress.org/Author_Templates',
+							 'author' => 'http://wpdocs.sourceforge.jp/Author_Templates',
 							
-							 'category' => 'http://codex.wordpress.org/Category_Templates',
+							 'category' => 'http://wpdocs.sourceforge.jp/Category_Templates',
 							
-							 'archive_posttype' => 'http://codex.wordpress.org/Post_Type_Templates',
+							 'archive_posttype' => 'http://wpdocs.sourceforge.jp/Post_Type_Templates',
 							
-							 'taxonomy' => 'http://codex.wordpress.org/Post_Type_Templates',
+							 'taxonomy' => 'http://wpdocs.sourceforge.jp/Post_Type_Templates',
 								
 							 'date' => 'http://codex.wordpress.org/User:Skippy/Date.php',
 								
-							 'tag' => 'http://codex.wordpress.org/Tag_Templates',
+							 'tag' => 'http://wpdocs.sourceforge.jp/Tag_Templates',
 							
 							 'attachment' => 'http://wpdocs.sourceforge.jp/Using_Image_and_File_Attachments#.E3.83.86.E3.83.BC.E3.83.9E.E3.81.A7.E3.81.AE.E4.BD.BF.E7.94.A8.E6.B3.95',
 								
-							 'single_posttype' => 'http://codex.wordpress.org/Post_Type_Templates',
+							 'single_posttype' => 'http://wpdocs.sourceforge.jp/Post_Type_Templates',
 								
-							 'single_post' => 'http://codex.wordpress.org/Post_Type_Templates', // pop-out could be an updated and condensed version of: http://justintadlock.com/archives/2008/12/06/creating-single-post-templates-in-wordpress
+							 'single_post' => 'http://wpdocs.sourceforge.jp/Post_Type_Templates', // pop-out could be an updated and condensed version of: http://justintadlock.com/archives/2008/12/06/creating-single-post-templates-in-wordpress
 								
-							 'page_id' => 'http://codex.wordpress.org/Page_Templates#Specialized_Page_Template',
+							 'page_id' => 'http://wpdocs.sourceforge.jp/Page_Templates#Specialized_Page_Template',
 								
-							 'archive' => 'http://codex.wordpress.org/Creating_an_Archive_Index#The_Archives_Page',
+							 'archive' => 'http://wpdocs.sourceforge.jp/Creating_an_Archive_Index#The_Archives_Page',
 								
 							 'single' => 'http://wpdocs.sourceforge.jp/Theme_Development#Single_Post_.28single.php.29',
 								
-							 'page' => 'http://codex.wordpress.org/Page_Templates',
+							 'page' => 'http://wpdocs.sourceforge.jp/Page_Templates',
 							
 							 'home' => 'http://wpdocs.sourceforge.jp/Template_Hierarchy#Examples',
 								
-							 'comments_popup' => 'http://codex.wordpress.org/Function_Reference/comments_popup_script',
+							 'comments_popup' => 'http://wpdocs.sourceforge.jp/%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88%E3%82%BF%E3%82%B0/comments_popup_script',
 								
 							 '404' => 'http://wpdocs.sourceforge.jp/Creating_an_Error_404_Page',
 								
 							 'search' => 'http://wpdocs.sourceforge.jp/Creating_a_Search_Page',
 
-							 'paged_true' => 'http://codex.wordpress.org/Function_Reference/is_paged',
+							 'paged_true' => 'http://wpdocs.sourceforge.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/is_paged',
 								
-							 'paged_false' => 'http://codex.wordpress.org/Function_Reference/is_paged',
+							 'paged_false' => 'http://wpdocs.sourceforge.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/is_paged',
 
 							 'index' => 'http://wpdocs.sourceforge.jp/Theme_Development#Index_.28index.php.29' ); ?>
